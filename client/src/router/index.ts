9c1f56dc1503330f3b041b5e9e0d032fb4016e58
didomizm1 <didomizm1@newpaltz.edu>
2023-03-03 03:14:07 -0500

    Routes updates

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,22 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import ExercisesView from '../views/ExercisesView.vue'
+import FriendsView from '../views/FriendsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    { path: '/home', name: 'home', component: HomeView },
+    { path: '/about', name: 'about', component: AboutView },
   ]
 })
 
