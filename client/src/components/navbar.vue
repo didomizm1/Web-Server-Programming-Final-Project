425e597c7f00e10e134c067ffbd9c2a029b03447
didomizm1 <didomizm1@newpaltz.edu>
2023-03-03 03:14:59 -0500

    Navbar update

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -13,31 +13,29 @@ function toggleMenu() {
 </script>
 
 <template>
-    <nav class="navbar is-primary">
-        <div class="container">
-            <div class="navbar-brand">
-                <a class="navbar-item" href=""> 
-                    <img src="./media/images/logos/sushifit-white-no-background.png" alt="SushiFit Logo" width="150">
-                </a>
-                <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="toggleMenu" >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+    <nav class="navbar is-danger py-5">
+        <div class="navbar-brand">
+            <RouterLink to="/home" class="navbar-item">
+                <img src="@/assets/logos/sushifit-white-no-background.png" alt="SushiFit Logo" width="150">
+            </RouterLink>
+            <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="toggleMenu" >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    
+        <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
+            <div class="navbar-start">
+                <RouterLink to="/home" class="navbar-item">Home</RouterLink>
+                <RouterLink to="/about" class="navbar-item">About</RouterLink>
             </div>
         
-            <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
-                <div class="navbar-start">
-                    <RouterLink to="/" class="navbar-item">Home</RouterLink>
-                    <RouterLink to="/about" class="navbar-item">About</RouterLink>
-                </div>
-            
-                <div class="navbar-end">
-                    <LoginBadge />
-                </div>
+            <div class="navbar-end">
+                <LoginBadge />
             </div>
-            
         </div>
+
     </nav>
 </template>
 
