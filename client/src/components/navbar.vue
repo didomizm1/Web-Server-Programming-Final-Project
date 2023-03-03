8bd414891c0b039c93a93e725ef4fb1602bf5e40
didomizm1 <didomizm1@newpaltz.edu>
2023-03-03 02:32:45 -0500

    Navbar updates

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,89 +1,44 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+import LoginBadge from './LoginBadge.vue';
 
-    const isMenuActive = false;
+const isMenuActive = ref(false);
+
+function toggleMenu() {
+    isMenuActive.value = !isMenuActive.value;
+    console.log({ isMenuActive })
+}
+
 </script>
 
 <template>
     <nav class="navbar is-primary">
         <div class="container">
             <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                
-                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="22" height="22" />
-                
-            </a>
-            <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="isMenuActive = !isMenuActive" >
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+                <a class="navbar-item" href=""> 
+                    <img src="./media/images/logos/sushifit-white-no-background.png" alt="SushiFit Logo" width="150">
+                </a>
+                <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="toggleMenu" >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         
             <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
-            <div class="navbar-start">
-
-                <RouterLink to="/" class="navbar-item">Home</RouterLink>
-                <RouterLink to="/about" class="navbar-item">About</RouterLink>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
-                    Docs
-                </a>
-                <div class="navbar-dropdown">
-                    <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                    Overview
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
-                    Modifiers
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                    Columns
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                    Layout
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                    Form
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                    Elements
-                    </a>
-                    <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                    Components
-                    </a>
+                <div class="navbar-start">
+                    <RouterLink to="/" class="navbar-item">Home</RouterLink>
+                    <RouterLink to="/about" class="navbar-item">About</RouterLink>
                 </div>
+            
+                <div class="navbar-end">
+                    <LoginBadge />
                 </div>
             </div>
-        
-            <div class="navbar-end">
-                <div class="navbar-item">
-                <div class="field is-grouped">
-                    <p class="control">
-                    <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                        <span class="icon">
-                        <i class="fab fa-twitter"></i>
-                        </span>
-                        <span>
-                        Tweet
-                        </span>
-                    </a>
-                    </p>
-                    <p class="control">
-                    <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip">
-                        <span class="icon">
-                        <i class="fas fa-download"></i>
-                        </span>
-                        <span>Download</span>
-                    </a>
-                    </p>
-                </div>
-                </div>
-            </div>
-            </div>
+            
         </div>
-      </nav>
+    </nav>
 </template>
 
 <style scoped>
