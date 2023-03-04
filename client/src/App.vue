16705cac8d1e211113c53c7c3a83a4fd5030e60d
didomizm1 <didomizm1@newpaltz.edu>
2023-03-04 05:31:51 -0500

    Added column layout for all pages in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,12 +6,28 @@ import Navbar from './components/Navbar.vue';
 <template>
   <Navbar />
 
-  <div class="container">
-    <RouterView />
+  <div class="columns is-centered is-gapless">
+
+    <div class="column has-background-danger-light">
+      
+    </div> 
+
+    <div class="column is-8">
+      <section class="hero has-background-white is-fullheight">
+        <RouterView />
+      </section>
+    </div>
+
+    <div class="column has-background-danger-light">
+      
+    </div>
+
   </div>
+    
+  
   
 </template>
 
 <style scoped>
-
+  
 </style>
