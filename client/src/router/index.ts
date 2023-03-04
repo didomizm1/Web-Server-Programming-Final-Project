9d46d624eb6cab267a328578362d5cc768522b5e
didomizm1 <didomizm1@newpaltz.edu>
2023-03-03 20:24:55 -0500

    Added FAQ View

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import FAQView from '../views/FAQView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -12,7 +13,8 @@ const router = createRouter({
   routes: [
     { path: '/home', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: AboutView },
-    { path: '/login', name: 'login', component: LoginView },
+    { path: '/faq', name: 'faq', component: FAQView },
+    { path: '/faq', name: 'faq', component: LoginView },
     { path: '/register', name: 'register', component: RegisterView },
     { path: '/profile', name: 'profile', component: ProfileView },
     { path: '/exercises', name: 'exercises', component: ExercisesView },
