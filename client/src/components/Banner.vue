c3d6af606475fca05af59e00f10e72d006345c6c
didomizm1 <didomizm1@newpaltz.edu>
2023-03-12 06:21:02 -0400

    Added banner component

---
+++ client/src/components/Banner.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="hero has-background-danger-dark my-6">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <p class="title">
+                    <slot></slot>
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.title {
+  font-family: 'SF Wasabi', sans-serif;
+  font-size: 3rem;
+}
+</style>
