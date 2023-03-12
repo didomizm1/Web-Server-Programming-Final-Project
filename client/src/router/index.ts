58ed8f43bd3f94706813174c153699af34b19132
didomizm1 <didomizm1@newpaltz.edu>
2023-03-12 08:24:51 -0400

    Home view routing update

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -12,7 +12,7 @@ import FriendsView from '../views/FriendsView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/home', name: 'home', component: HomeView },
+    { path: '/', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: AboutView },
     { path: '/faqs', name: 'faqs', component: FAQsView },
     { path: '/login', name: 'login', component: LoginView },
