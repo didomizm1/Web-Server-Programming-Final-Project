c7b713de0d315ebf7a300458f1c3e8003b5ffb85
didomizm1 <didomizm1@newpaltz.edu>
2023-03-12 02:22:34 -0400

    Added new font

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -3,11 +3,34 @@
 </script>
 
 <template>
-  <div>
+<div>
+  <section class="hero has-background-danger-dark my-6">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="title">
+          Welcome back!
+        </p>
+      </div>
 
+    </div>
+  </section>
+  
+  <div class="container form">
+    <div class="box">
+      
+
+    </div>
   </div>
+  
+</div>
 </template>
 
 <style scoped>
-
+.title {
+  font-family: 'SF Wasabi', sans-serif;
+  font-size: 3rem;
+}
+.form {
+  width: 50rem;
+}
 </style>
