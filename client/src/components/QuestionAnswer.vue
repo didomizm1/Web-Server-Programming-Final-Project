6a3ce38ae917c5322ae01facc2140cc7e508a22b
didomizm1 <didomizm1@newpaltz.edu>
2023-03-12 10:28:28 -0400

    FAQs view updates, created question/answer component

---
+++ client/src/components/QuestionAnswer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="section">
+        <h1>
+            <slot name="q"></slot>
+        </h1>
+        <p>
+            <slot name="a"></slot>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- client/src/views/FAQsView.vue
+++ client/src/views/FAQsView.vue
@@ -1,11 +1,75 @@
 <script setup lang="ts">
-
+import Banner from '../components/Banner.vue';
+import CustomBox from '../components/CustomBox.vue';
+import QuestionAnswer from '../components/QuestionAnswer.vue';
 </script>
 
 <template>
-  <div>
+  <Banner>
+    <template #title>
+      Frequently Asked Questions
+    </template>
+  </Banner>
 
-  </div>
+  <CustomBox>
+    <div class="content">
+      <QuestionAnswer>
+        <template #q>
+          Question 1?
+        </template>
+        <template #a>
+          Answer 1.
+        </template>
+      </QuestionAnswer>
+
+      <QuestionAnswer>
+        <template #q>
+          Question 2?
+        </template>
+        <template #a>
+          Answer 2.
+        </template>
+      </QuestionAnswer>
+
+      <QuestionAnswer>
+        <template #q>
+          Question 3?
+        </template>
+        <template #a>
+          Answer 3.
+        </template>
+      </QuestionAnswer>
+
+      <QuestionAnswer>
+        <template #q>
+          Question 4?
+        </template>
+        <template #a>
+          Answer 4.
+        </template>
+      </QuestionAnswer>
+
+      <QuestionAnswer>
+        <template #q>
+          Question 5?
+        </template>
+        <template #a>
+          Answer 5.
+        </template>
+      </QuestionAnswer>
+
+      <QuestionAnswer>
+        <template #q>
+          Question 6?
+        </template>
+        <template #a>
+          Answer 6.
+        </template>
+      </QuestionAnswer>
+
+    </div>
+  </CustomBox>
+
 </template>
 
 <style scoped>
