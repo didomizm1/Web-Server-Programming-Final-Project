592d44e0221a5253c005c682cae0e2b2d39c5a1f
didomizm1 <didomizm1@newpaltz.edu>
2023-03-15 07:10:24 -0400

    Added session.ts and model folder

---
+++ client/src/model/session.ts
@@ -0,0 +1,46 @@
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+
+// Reactive session object
+const session = reactive({
+    user: null as User | null,
+});
+
+// Basic blueprint for a user
+interface User {
+    userID: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    birthday: string;
+    token?: string;
+}
+
+// Function to use the session
+export function useSession() {
+    return session;
+}
+
+// Login function
+export function login() {
+    session.user = {
+        userID: 0,
+        username: "testuser",
+        email: "testemail@email.com",
+        firstName: "Test",
+        lastName: "User",
+        birthday: "0/0/0000",
+    }
+}
+
+// Logout function, redirects to login page
+export function useLogout() {
+    const router = useRouter()
+
+    return function(){
+        console.log({router});
+        session.user = null;
+        router.push("/login");
+    }
+}
