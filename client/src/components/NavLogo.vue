79e55ecee71f82ef5c1204aabb372bfef47ddc8c
didomizm1 <didomizm1@newpaltz.edu>
2023-03-16 21:55:02 -0400

    Added logo component for navbar and footer

---
+++ client/src/components/NavLogo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <!-- Component which simplifies insertions of the navigation site logo -->
+    <img src="../assets/logos/sushifit-white-no-background.png" alt="SushiFit Logo" width="150">
+</template>
+
+<style scoped>
+
+</style>
--- client/src/components/SiteLogo.vue
+++ client/src/components/SiteLogo.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+    <!-- Component which simplifies insertions of the main site logo -->
     <img src="../assets/logos/sushifit-high-resolution-color-logo.png" alt="SushiFit Logo: A new way to experience fitness.">
 </template>
 
