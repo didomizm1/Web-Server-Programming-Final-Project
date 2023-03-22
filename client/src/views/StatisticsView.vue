e9b20b37fadd95f051f399f4c7d6a190fb128f16
didomizm1 <didomizm1@newpaltz.edu>
2023-03-21 23:07:37 -0400

    Created statistics box component for statistics view

--- client/src/views/StatisticsView.vue
+++ client/src/views/StatisticsView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Banner from '../components/Banner.vue';
+import StatsBox from '../components/StatsBox.vue';
 </script>
 
 <template>
@@ -12,6 +13,22 @@ import Banner from '../components/Banner.vue';
             View detailed stats about your routine!
         </template>
     </Banner>
+
+    <!-- Statistics for different periods of time -->
+    <!-- Today -->
+    <StatsBox>
+
+    </StatsBox>
+
+    <!-- This week -->
+    <StatsBox>
+        
+    </StatsBox>
+
+    <!-- All time -->
+    <StatsBox>
+        
+    </StatsBox>
 
 </template>
 
