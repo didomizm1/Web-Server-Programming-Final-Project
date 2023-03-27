190ebcf1b98aaa6878e37d031205ed5a5aadde53
didomizm1 <didomizm1@newpaltz.edu>
2023-03-27 02:42:48 -0400

    Server update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
-const http = require('http');
+const express = require('express')
+const path = require('path')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello New York Rangers!');
-});
+// Middleware
+app
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
 
-server.listen(port, () => 
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello New York Rangers! From Express')
+    })
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+
+app.listen(port, () => 
   console.log(`Server running at http://${hostname}:${port}/`)
 );
