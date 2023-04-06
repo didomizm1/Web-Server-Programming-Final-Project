b6126afb2581881b7c7bd7d14bc954e19b8b441d
didomizm1 <didomizm1@newpaltz.edu>
2023-04-06 02:24:39 -0400

    Created myFetch

---
+++ client/src/model/myFetch.ts
@@ -0,0 +1,32 @@
+const API_URL = 'http://localhost:3000/api/v1/';
+
+// REST API fetch functionality
+export function rest(url: string, data?: any, method?: string, headers?: any){
+    return fetch(url, {
+        method: method ?? (data ? 'POST' : 'GET'),
+        headers: {
+            'Content-Type': 'application/json',
+            ...headers,
+        },
+        body: data ? JSON.stringify(data) : undefined,
+    })
+        .then(res => res.ok 
+            ? res.json() 
+            : res.json().then(x=> { throw({ ...x, message: x.error }) } )
+        );
+}
+
+export function api(url: string, data?: any, method?: string, headers?: any){
+    return rest(API_URL + url, data, method, headers);
+}
+
+// Data envelopes
+export type DataEnvelope<T> = {
+    data: T,
+    isSuccess: boolean,
+    error?: string,
+}
+
+export type DataListEnvelope<T> = DataEnvelope<T[]> & {
+    total: number,
+}
