0d48a1e1c40149d9e4719b4460225d171d77e13c
didomizm1 <didomizm1@newpaltz.edu>
2023-04-06 01:47:01 -0400

    Added actions, error handling, and logging to index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require('path')
+const users = require('./controllers/users')
+const exercises = require('./controllers/exercises')
+const workouts = require('./controllers/workouts')
 const app = express()
 
 const hostname = '127.0.0.1';
@@ -10,18 +13,45 @@ app
     .use(express.json())
     .use(express.static(path.join(__dirname, '../client/dist')))
 
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        next()
+    })
+
+
 // Actions
 app
     .get('/api/v1/', (req, res) => {
         res.send('Hello New York Rangers! From Express')
     })
+    .use('/api/v1/users', users)
+    .use('/api/v1/exercises', exercises)
+    .use('/api/v1/workouts', workouts)
 
 // Catch all
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
 
+// Error handling
+app
+    .use((err, req, res, next) => {
+        console.error(err);
+        const msg = {
+            status: err.code || 500,
+            error: err.message || 'Internal Server Error',
+            isSuccess: false
+        }
+        res.status(msg.status).json(msg)
+    })
+
+// Console logging
+console.log('1: About to start server')
 
 app.listen(port, () => 
-  console.log(`Server running at http://${hostname}:${port}/`)
-);
+  console.log(`2: Server running at http://${hostname}:${port}/`)
+)
+
+console.log('3: Asked server to start')
