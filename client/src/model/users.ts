41c41c15c74b21fd011c56b4fd88bf4cbd23049b
didomizm1 <didomizm1@newpaltz.edu>
2023-04-20 14:15:58 -0400

    Added password to user

--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -5,6 +5,7 @@ import { api } from "./session";
 export interface User {
     id: number;
     username: string;
+    password: string;
     email: string;
     firstName: string;
     lastName: string;
