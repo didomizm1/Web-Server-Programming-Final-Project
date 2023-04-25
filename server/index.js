f8065a21e854ad8f6905826e36048d71fa858391
didomizm1 <didomizm1@newpaltz.edu>
2023-04-24 23:28:12 -0400

    Updated port number and scratch testing

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { requireLogin } = require('./middleware/authorization')
 const app = express()
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3070;
 
 // Middleware
 app
